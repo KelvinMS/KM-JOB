3f57836dd4f570323ff9213abcb15df6a5d191db
kms <kelvin651b@gmail.com>
2025-05-09 14:52:15 -0300

    widgets for car parts

--- tabs/orcamentos_widgets.py
+++ tabs/orcamentos_widgets.py
@@ -2,10 +2,13 @@ import customtkinter as ctk
 from tkinter import ttk, messagebox
 
 
+
 class OrcamentosWidgets(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
         self.frames = {}
+        self.checkbox_vars = []
+        self.entry_carinfo_vars = []
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
         self.frame_action_buttons_orcamento()
@@ -34,7 +37,7 @@ class OrcamentosWidgets(ctk.CTkFrame):
     def frame_vehicle_data_entrys(self):
         self.frames["frame_vehicle_data_entrys"] = ctk.CTkFrame(master=self, width=300, height=300, fg_color="gray22")
         self.frames["frame_vehicle_data_entrys"].grid(row=0, column=1, padx=5, pady=5, sticky="nwe")
-        #self.frames["frame_vehicle_data_entrys"].grid_columnconfigure(1, weight=1)
+
 
 
         cliente_data_entrys = [
@@ -51,18 +54,54 @@ class OrcamentosWidgets(ctk.CTkFrame):
         lbl_info_veiculo.rowconfigure(0, weight=1)
         # Adiciona os labels e entries para cada item na lista cliente_data_entrys
         for i, (label_text, _) in enumerate(cliente_data_entrys):
+            var = ctk.StringVar() # Controla se está marcado (1) ou não (0)
             ctk.CTkLabel(master=self.frames["frame_vehicle_data_entrys"], text=label_text + ":")\
                 .grid(row=i+1, column=0, padx=10, pady=5, sticky="e")
-            entry = ctk.CTkEntry(master=self.frames["frame_vehicle_data_entrys"], width=200, placeholder_text=label_text)
+            entry = ctk.CTkEntry(master=self.frames["frame_vehicle_data_entrys"], width=200, placeholder_text=label_text,textvariable=var)
             entry.grid(row=i+1, column=1, padx=10, pady=5, sticky="ew")
+            self.entry_carinfo_vars.append(var)
+
+
+        self.widget_car_parts_data_entrys()
 
         # Última linha para textbox de descrição
-        ultima_linha = self.get_ultima_linha(self.frames["frame_vehicle_data_entrys"])
+        ultima_linha = self.get_last_row(self.frames["frame_vehicle_data_entrys"])
         ctk.CTkLabel(master=self.frames["frame_vehicle_data_entrys"], text="Descrição:")\
-            .grid(row=ultima_linha, column=0, padx=10, pady=5, sticky="ne")
+            .grid(row=ultima_linha, column=0, padx=10, pady=5, sticky="nw")
         descricao = ctk.CTkTextbox(master=self.frames["frame_vehicle_data_entrys"], width=200, height=100)
         descricao.grid(row=ultima_linha, column=1, padx=10, pady=5, sticky="ew")
 
+
+    def widget_car_parts_data_entrys(self):
+        # Adiciona os labels e entries para cada item na lista cliente_data_entrys
+        car_parts = [
+            "Parachoque Dianteiro",
+            "Parachoque Trasero",
+            "Paralama Dianteiro Direito",
+            "Paralama Dianteiro Esquerdo",
+            "Paralama Trasero Direito",
+            "Paralama Trasero Esquerdo",
+            "Capô",
+            "Porta Dianteira Direita",
+            "Porta Dianteira Esquerda",
+            "Porta Trasera Direita",
+            "Porta Trasera Esquerda",
+            "Teto"
+        ]
+        ultima_linha = self.get_last_row(self.frames["frame_vehicle_data_entrys"])
+        self.frames['frame_choose_body_part'] = ctk.CTkFrame(self.frames["frame_vehicle_data_entrys"], width=300, height=300, fg_color="transparent")
+        self.frames['frame_choose_body_part'].grid(row=ultima_linha, column=0, sticky="new",columnspan=2)
+        self.frames['frame_choose_body_part'].grid_rowconfigure(0, weight=1)
+        ctk.CTkLabel(master=self.frames['frame_choose_body_part'] , text="Selecionar Peças para Orçamento", font=("Arial", 16),text_color="DimGrey",anchor="center")\
+            .grid(row=0, column=0, padx=10, pady=5, columnspan=2, sticky="new")
+        
+        for i, label_text in enumerate(car_parts,start=1):
+            linha = ((i - 1) % 6) + 1   
+            coluna = (i - 1) // 6
+            var = ctk.IntVar()  # Controla se está marcado (1) ou não (0)
+            chk = ctk.CTkCheckBox(self.frames['frame_choose_body_part'], text=label_text, variable=var)
+            chk.grid(row=linha, column=coluna, sticky="w", padx=10, pady=5)
+            self.checkbox_vars.append(var)
 
 
 
@@ -83,7 +122,7 @@ class OrcamentosWidgets(ctk.CTkFrame):
         
         # Adiciona os Comboboxes para selecionar um cliente existente
         clientes = ["Cliente 1", "Cliente 2", "Cliente 3"]
-        ultima_linha = self.get_ultima_linha(self.frames["frame_client_data_entrys"])
+        ultima_linha = self.get_last_row(self.frames["frame_client_data_entrys"])
         ctk.CTkLabel(master=self.frames["frame_client_data_entrys"], text="Selecionar Cliente:")\
             .grid(row=ultima_linha, column=0, padx=10, pady=5, sticky="e")
         self.combo_clientes = ctk.CTkComboBox(master=self.frames["frame_client_data_entrys"], values=clientes, width=200)
@@ -91,14 +130,14 @@ class OrcamentosWidgets(ctk.CTkFrame):
         self.combo_clientes.set("Selecione um cliente") 
 
         # Checkbox para novo cliente
-        ultima_linha = self.get_ultima_linha(self.frames["frame_client_data_entrys"])
+        ultima_linha = self.get_last_row(self.frames["frame_client_data_entrys"])
 
         self.novo_cliente_var = ctk.BooleanVar(value=False)
         self.checkbox_novo_cliente = ctk.CTkCheckBox(self.frames["frame_client_data_entrys"], text="Cadastrar novo cliente", variable=self.novo_cliente_var,command=self.widgets_new_client_data_entrys)
         self.checkbox_novo_cliente.grid(row=ultima_linha, column=0, padx=10, pady=5)
 
         # divider entre os widgets
-        ultima_linha = self.get_ultima_linha(self.frames["frame_client_data_entrys"])
+        ultima_linha = self.get_last_row(self.frames["frame_client_data_entrys"])
         divider = ctk.CTkFrame(self.frames["frame_client_data_entrys"], height=2, fg_color="DarkSlateGray")
         divider.grid(row=ultima_linha, column=0, sticky="ew", pady=5,padx=(5,5),columnspan=2)
 
@@ -117,7 +156,7 @@ class OrcamentosWidgets(ctk.CTkFrame):
                 ("Telefone", "entry_phone"),
                 ("Email", "entry_email")
             ]
-            ultima_linha = self.get_ultima_linha(self.frames["frame_client_data_entrys"])
+            ultima_linha = self.get_last_row(self.frames["frame_client_data_entrys"])
             self.frames['frame_add_new_client'] = ctk.CTkFrame(self.frames["frame_client_data_entrys"], width=300, height=300, fg_color="transparent")
             self.frames['frame_add_new_client'].grid(row=ultima_linha, column=0, sticky="new",columnspan=2)
             
@@ -132,14 +171,14 @@ class OrcamentosWidgets(ctk.CTkFrame):
                 entry.grid(row=i+1, column=1, padx=10, pady=5, sticky="ew")
 
             # Última linha para textbox de descrição
-            ultima_linha = self.get_ultima_linha(self.frames["frame_client_data_entrys"])
+            ultima_linha = self.get_last_row(self.frames["frame_client_data_entrys"])
             ctk.CTkLabel(master=self.frames['frame_add_new_client'] , text="Descrição:")\
                 .grid(row=ultima_linha, column=0, padx=10, pady=5, sticky="ne")
             descricao = ctk.CTkTextbox(master=self.frames['frame_add_new_client'] , width=200, height=100)
             descricao.grid(row=ultima_linha, column=1, padx=10, pady=5, sticky="ew")
 
     # Método para obter a última linha ocupada em um frame
-    def get_ultima_linha(self,master):
+    def get_last_row(self,master):
         widgets = master.grid_slaves()
         if not widgets:
             return 0
@@ -154,7 +193,6 @@ class OrcamentosWidgets(ctk.CTkFrame):
     # Método para consultar um orçamento
     def consultar_orcamentos(self):
         # Implementar a lógica para criar um orçamento
-        print("Consultar Orçamento Clicked")
         self.frames["frame_entrys_orcamentos"].forget()
         pass
 
